74003103de5b206c4b91938a52ce40b2e7260821
sukumar <108935834+Limitless360@users.noreply.github.com>
2022-12-24 21:21:33 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:sys_chat/screens/complete_profile_page.dart';
+import 'package:sys_chat/screens/home_page.dart';
+import 'package:sys_chat/screens/login_page.dart';
+import 'package:sys_chat/screens/splashhome_page.dart';
+import 'package:sys_chat/screens/splashlogin_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:uuid/uuid.dart';
+import 'firebase_options.dart';
+
+var uuid = Uuid();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: AuthStateChanges(),
+      builder: EasyLoading.init(),
+    );
+  }
+}
+
+AuthStateChanges() {
+  return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          return SplashPageHome();
+        } else {
+          return SplashLoginPage();
+        }
+      });
+}
